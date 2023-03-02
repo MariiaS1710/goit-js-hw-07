be7c483244570ae3dcad44170a4382036819d797
mariia <111996419+MariiaS1710@users.noreply.github.com>
2023-03-02 22:39:07 +0100

    js\01,js\02 ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryItemsEl = galleryItems.map(el => {
+    return `<div class="gallery__item"> 
+    <a class = "gallery__link" href="${el.original}"> 
+    <img class = "gallery__image" data-sourse = "${el.original}" src="${el.preview}" alt="${el.description}"/>
+    </a>
+    </div>`
+})
+    .join("");
+
+galleryContainer.insertAdjacentHTML('beforeend',  galleryItemsEl);
+
+galleryContainer.addEventListener('click', onImg);
+
+function onImg(ev) {
+    ev.preventDefault();
+    if (ev.target.nodeName !== 'IMG')     return
+    const imgSelected = ev.target.getAttribute('data-sourse');
+    // console.log(imgSelected)
+    
+    
+    const galleryModal = basicLightbox.create(`
+    <img src = "${imgSelected}" width = '600' height = '400'/> 
+`)
+    
+galleryModal.show() 
+};
+
+    
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryItemsEl = galleryItems.map(el => {
+    return `<a class = "gallery__item" href="${el.original}"> 
+    <img class = "gallery__image" src="${el.preview}" alt="${el.description}" title = "${el.description}"/>
+    </a>
+    `
+})
+    .join("");
+
+
+galleryContainer.insertAdjacentHTML('beforeend',  galleryItemsEl);
+
+galleryContainer.addEventListener('click', onImg);
+
+function onImg(ev) {
+    ev.preventDefault();
+    if (ev.target.nodeName !== 'IMG')     return
+    const imgSelected = ev.target.getAttribute('data-sourse');
+    // console.log(imgSelected)
+    var lightbox = new SimpleLightbox('.gallery a', {
+        captionDelay: 250,
+    });
+
+  };
